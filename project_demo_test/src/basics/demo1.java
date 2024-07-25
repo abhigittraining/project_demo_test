83984cbfb8c3b1bb4deab00c700711566a6cb2d5
abhishek <abhishek@DESKTOP-BGL411A>
2024-07-25 16:14:05 +0530

    pushing changed code

--- project_demo_test/src/basics/demo1.java
+++ project_demo_test/src/basics/demo1.java
@@ -5,6 +5,10 @@ public class demo1 {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 10;
-		System.out.println(a+b);
+		System.out.println(a + b);
+
+		int x = 100;
+		int y = 10;
+		System.out.println(x - y);
 	}
 }
