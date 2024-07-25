4372329ce44db53c637a264d5e3b55e9cf7917b7
abhishek <abhishek@DESKTOP-BGL411A>
2024-07-25 16:18:12 +0530

    commit 3 adding demo3.java file

---
+++ project_demo_test/src/basics/demo2.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class demo2 {
+
+	public void addition() {
+		int a = 100;
+		int b = 100;
+		int sum = a + b;
+		System.out.println(sum);
+	}
+	
+	public void subtraction() {
+		int a = 100;
+		int b = 50;
+		int sum = a - b;
+		System.out.println(sum);
+	}
+	
+	public static void main(String[] args) {
+		demo2 obj = new demo2();
+		obj.addition();
+		obj.subtraction();
+	}
+}
