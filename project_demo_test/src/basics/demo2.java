a6afb1feb964cc9544478020c745e0caa63fb2a6
abhishek <abhishek@DESKTOP-BGL411A>
2024-07-25 16:27:59 +0530

    demo2 file changed

--- project_demo_test/src/basics/demo2.java
+++ project_demo_test/src/basics/demo2.java
@@ -5,15 +5,31 @@ public class demo2 {
 	public void addition() {
 		int a = 100;
 		int b = 100;
-		int sum = a + b;
-		System.out.println(sum);
+		int add = a + b;
+		System.out.println(add);
 	}
 	
 	public void subtraction() {
 		int a = 100;
 		int b = 50;
-		int sum = a - b;
-		System.out.println(sum);
+		int sub = a - b;
+		System.out.println(sub);
+		System.out.println("hello");
+	}
+	
+	public void multiplication() {
+		int a = 100;
+		int b = 50;
+		int multi = a * b;
+		System.out.println(multi);
+		System.out.println("hello");
+	}
+	
+	public void division() {
+		int a = 100;
+		int b = 50;
+		int div = a / b;
+		System.out.println(div);
 		System.out.println("hello");
 	}
 	
